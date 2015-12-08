f6aa1027820a9b7d394db0f5bf13dd6125853ec6
Joseph Morrison <jmorri35@gmail.com>
2015-12-08 16:41:23 +0000

    Added package to make post work with parameters

--- routes/submit.js
+++ routes/submit.js
@@ -2,6 +2,8 @@ var express = require('express');
 var querystring = require('querystring');
 var router = express.Router();
 var http = require('http');
+var dot = require('dot-object');
+var request = require('request');
 
 router.get('/', function (req, res, next) {
 	console.log('a get');
@@ -16,6 +18,8 @@ router.post('/', function(req, res, next) {
 	var end = new Date('December 18, 2015 20:25:00');
 	var etime = 'Europe/London';
 	var currency = req.body.currency;
+
+	var obj
 	var event = {
 		name : {
 			html : name
@@ -31,36 +35,13 @@ router.post('/', function(req, res, next) {
 		currency : currency
 	};
 	var htmlData;
-	// post data
-	var post_options = {
-		host: 'https://www.eventbriteapi.com',
-		port: 80,
-		path: '/v3/events/?token=OVJYRQ3E5UCTVNZGU5OP',
-		method: 'POST',
-		headers : {
-			'Content-Type' : 'application/x-www-form-urlencoded',
-			'Content-Length' : Buffer.byteLength(JSON.stringify(event))
-		}
-	};
 
-
-
-	var httpreq = http.request(post_options, function (response) {
-		//console.log(response);
-		//response.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-		response.on('data', function(chunk) {
-			htmlData = chunk;
-			console.log('%s', chunk);
-			res.write(htmlData);
-		});
-		response.on('end', function () {
-			res.send('ok')
-		});
+	request.post({
+		url : 'https://www.eventbriteapi.com/v3/events/?token=OVJYRQ3E5UCTVNZGU5OP&event.name.html='+ encodeURIComponent(name) +'&event.start.utc=2015-12-13T10:39:35Z&event.start.timezone=Europe/London&event.end.utc=2015-12-13T10:39:35Z&event.currency='+ encodeURIComponent(currency) +'&event.end.timezone=Europe/London'
+	}, function(error, response, body) {
+		console.log('error: %s response: %s body: %s', error, response, body);
+		res.end(body)
 	});
-	httpreq.write(JSON.stringify(event));
-  	httpreq.end();
-
-	
 
 	console.log('name: %s, start: %s, stime: %s, end: %s, etime: %s, currency: %s', name, start, stime, end, etime, currency);
 });
